3284277b8bac26cd3f3dd588e4fa596cad2cb5b5
Murad A. Jabiyev <jabievmurad@gmail.com>
2026-06-21 19:15:23 +0400

    Delete KERNEL/C/externASM.h

--- KERNEL/C/externASM.h
+++
@@ -1,8 +0,0 @@
-extern void PIANO_MAIN();
-extern void PIANO_DESTROY();
-extern void STORE_BACKBUF();
-extern void LOAD_SCREEN();
-extern void random_hw();
-extern void rdrandU8();
-extern void rdrandU16();
-extern void rdrandU32();
