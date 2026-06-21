60fccba9c733681f76d1f824f6be2bb2d990a230
Murad A. Jabiyev <jabievmurad@gmail.com>
2026-06-21 18:01:07 +0400

    Add files via upload

---
+++ KERNEL/ASM/Functions.asm
@@ -0,0 +1,90 @@
+global rdrandU8
+global rdrandU16
+global rdrandU32
+global random_hw
+global testif
+global KMemcpy
+global KMemset
+global ComplementBit
+testif:
+	push ebp
+	mov ebp,esp
+	xor edx,edx
+	mov eax,1
+	cpuid
+	pop ebp
+	ret
+random_hw:
+	mov edi,0x8000
+	rdrand ax
+	and ax,0xff
+	mov word [edi],ax
+	ret
+rdrandU8:
+	push edi
+	push eax
+	mov edi,0x8000
+	rdrand ax
+	and ax,0xff
+	mov byte [edi],al
+	pop eax
+	pop edi
+	ret
+rdrandU16:
+	push edi
+	push eax
+	mov edi,0x8000
+	rdrand ax
+	mov word [edi],ax
+	pop eax
+	pop edi
+rdrandU32:
+	push edi
+	push eax
+	mov edi,0x8000
+	rdrand eax
+	mov dword [edi],eax
+	pop eax
+	pop edi
+	ret
+KMemset:
+	push ebp
+	mov ebp,esp
+	push eax
+	push ecx
+	push edi
+	mov dword edi,[ebp+8]
+	mov ebx,edi
+	mov byte al,[ebp+12]
+	mov dword ecx,[ebp+16]
+	rep stosb
+	pop edi
+	pop ecx
+	pop eax
+	pop ebp
+	mov eax,ebx
+	ret
+KMemcpy:
+	push ebp
+	mov ebp,esp
+	push edi
+	push esi
+	push ecx
+	mov dword esi,[ebp+8]
+	mov dword edi,[ebp+12]
+	mov eax,edi
+	mov dword ecx,[ebp+16]
+	rep movsb
+	pop ecx
+	pop esi
+	pop edi
+	pop ebp
+	ret
+ComplementBit:
+	push ebp
+	mov ebp,esp
+	mov byte al,[ebp+12]
+	movzx eax,al
+	btc dword [ebp+8],eax
+	pop ebp
+	ret
