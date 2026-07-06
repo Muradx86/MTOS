fcf984a2c475fe914b0e1d1c674bcc051d1fb619
Murad A. Jabiyev <jabievmurad@gmail.com>
2026-07-06 22:23:08 +0400

    Add files via upload

--- KERNEL/C/keyboard.c
+++ KERNEL/C/keyboard.c
@@ -4,6 +4,7 @@
 #include "port.h"
 #include "keyboard.h"
 #include "util.h"
+#include "ExternASM.h"
 #define PS2_SC 0x60
 #define PS2_R 0x64
 #define MAX 20
@@ -14,18 +15,22 @@
 #define LCTRL 0x1D
 #define BKSPC 0xE
 #define ENTER 0x1C
-extern void PIANO_MAIN();
+extern void readHymn();
+struct Mode{
+	uint8_t hymn;
+};
+struct Mode Mod;
 const char lookup_norml[] = {
 	1,27,'1','2','3','4','5','6','7','8','9','0','-','=',
 	'\b',
-	0,'q',     
+	'\t','q',     
 	'w','e','r','t','y','u',
 	'i','o','p','[',']','\n',
-	0,'a','s','d',
+	0, 'a','s','d',
 	'f','g','h','j','k','l',
-	';','"',0,
+	';',0x27,0,
 	0,43,'z','x','c','v','b','n','m',',','.',92,
-	0,0,0,32,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,32,0,0,0,0,0,0,0,0,0,0,0,
 	0,0,0,0,0x48,0,0,0x4B,0,0x4D,0,0,0x50,0
 };
 const char lookup_shift[] = {
@@ -36,30 +41,42 @@ const char lookup_shift[] = {
 	'I','O','P','{','}','|',
 	0,'A','S','D',
 	'F','G','H','J','K','L',
-	':',0,0,
+	':','"',0,
 	0,43,'Z','X','C','V','B','N','M','<','>','|',
-	'?',0,0,32,0,0,0xE3,0xE9,0,0,0,0,0,0,
-	0,0,0,0x48,0,0,0x4B,0,0x4D,0,0,0x50,0,0
+	 '?',0,0,32,0,0,0xE3,0xE9,0,0,0,0,0,
+	0,0,0,0,0x48,0,0,0x4B,0,0x4D,0,0,0x50,0
 };
+void BufWrite(char* buf,uint32_t n,char scancode)
+{
+	char* addres = (char*)(buf + n);
+	*addres = scancode; 
+}
 static inline void corner_print(uint8_t scancode,const char* tbl)
 {
 	*(volatile uint8_t*)CORNER8 = *(tbl + scancode);
-	*(volatile uint8_t*)0xb809f = 0x8f;
+	*(volatile uint8_t*)0xb809f = COLOR;
 }
-volatile bool shift_clicked = false;
-volatile bool ctrl_clicked = false;
-volatile bool backspace = false; 
+char getch(void)
+{
+	return lookup_norml[inb(PS2_SC)];
+}
+char* getln(char* s)
+{
+	static const uint32_t limit = 80;
+	uint32_t i;
+	char c;
+	for(i=0;i<limit-1 &&(c=getch())!='\n';++i)
+		s[i] = c;
+	if(c=='\n')
+		s[i] = '\0';
+	return s;
+}
+bool shift_clicked = false;
+bool ctrl_clicked = false;
+bool backspace = false;
 
 void keyboard_handler(void)
 {	
-	struct Kbd{
-		void (*pCP)(uint8_t,const char*);
-		volatile uint16_t* (*pPUTCHAR)(char);
-	};
-	struct Kbd* Kbd;
-	Kbd->pCP = &corner_print;
-	Kbd->pPUTCHAR = &vga_putchar;
-	
 	volatile uint8_t scancode_tbl = inb(PS2_SC);
 	volatile uint8_t scancode_raw;
 	
@@ -67,45 +84,40 @@ void keyboard_handler(void)
 
 	switch (scancode_tbl){//Rshift 0x36 Lshift 0x2A
 		case LSHIFT:
-			shift_clicked = clicked; coordinate_print("SHIFT",75,0); break;
+			shift_clicked = clicked; 
+			break;
 		case RSHIFT:
-			shift_clicked = clicked; coordinate_print("SHIFT",75,0); break;
+			shift_clicked = clicked; 
+			break;
 		case 0xAA:
-			shift_clicked = false; kmemset((void*)0xb8096,10,0x33); break;
+			shift_clicked = false;
+			break;
 		case 0xB6:
-			shift_clicked = false; kmemset((void*)0xb8096,10,0x33); break;
+			shift_clicked = false;
+			break;
 		case LCTRL:
-			ctrl_clicked = clicked; coordinate_print("CTRL",74,0); break;
+			ctrl_clicked = clicked;
+			break;
 		case 0x9D:
-			ctrl_clicked = false; kmemset((void*)0xb8096,10,0x33); break;
+			ctrl_clicked = false;
+			 break;
 		case 0xE:
-			backspace = clicked; coordinate_print("BCKSPC",74,0); break;
+			backspace = clicked; 
+			break;
 		case 0x8E:
-			backspace = false; kmemset((void*)0xb8094,12,0x33); break;
-		case ENTER:
-			coordinate_print("ENTER",75,0); break;
-		case 0x9C:
-			kmemset((void*)0xb8096,10,0x33); break;
-		case 0x58:
-			PIANO_MAIN();
+			backspace = false; 
 			break;
 		default:
 			break;
 	}
  	if(clicked && shift_clicked && scancode_tbl != LSHIFT && scancode_tbl != RSHIFT){//Shift letters
  		scancode_raw = lookup_shift[scancode_tbl]; 		
-		(*vga_putchar)(scancode_raw);
-		(*corner_print)(scancode_tbl,lookup_shift);
+		vga_putchar(scancode_raw);
+		corner_print(scancode_tbl,lookup_shift);
  	}
  	else if(clicked && scancode_tbl != LSHIFT && scancode_tbl != LCTRL){//Normal letters
  		scancode_raw = lookup_norml[scancode_tbl];
-		(*vga_putchar)(scancode_raw);
-		(*corner_print)(scancode_tbl,lookup_shift);
-
- 	}
- 	else if(clicked && shift_clicked && ctrl_clicked && scancode_tbl != LSHIFT && scancode_tbl != RSHIFT && scancode_tbl != LCTRL){//scientific
- 		scancode_raw = lookup_shift[scancode_tbl];		
-		(*vga_putchar)(scancode_raw);
-		(*corner_print)(scancode_tbl,lookup_shift);
+		vga_putchar(scancode_raw);
+		corner_print(scancode_tbl,lookup_norml);
  	}
 }
